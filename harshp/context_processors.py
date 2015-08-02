c141e26745c90a2c01160c0d10eb8656d2248022
Harshvardhan Pandit <coolharsh55@gmail.com>
2015-08-02 13:12:26 +0530

    new: clean, minimalist look powered by simple-css & font 'Josephin Slab'
    The site no longer uses individual color shades for apps. It uses
    color (#EEE) for the background, and uniform styling across the website.
    The font Josefin Slab is used in all text, with the font size ramped
    up from 125% at the start. Bootstrap elements have been refactored to
    remove any styling to make them feel uniform on the page. Mobile page
    optimizations have been done in respect to page and font size, and
    article placements. General code cleaning and minor refactoring.

--- harshp/context_processors.py
+++ harshp/context_processors.py
@@ -1,8 +1,14 @@
+"""context processors
+"""
 from django.contrib.sites.models import Site
-from django.contrib.sites.models import get_current_site
 from django.utils.functional import SimpleLazyObject
 
 
 def site(request):
-  return { 'site': SimpleLazyObject(lambda: 'http://%s' % Site.objects.get_current().domain),
-	}
+    """get site object
+    """
+    return {
+        'site': SimpleLazyObject(
+            lambda: 'http://%s' % Site.objects.get_current_site().domain
+        ),
+    }
--- harshp/views.py
+++ harshp/views.py
@@ -1,5 +1,6 @@
-# VIEWS
-from django.http import HttpResponse
+"""views for harshp
+"""
+
 from django.http import Http404
 from django.shortcuts import render_to_response
 
@@ -9,35 +10,89 @@ from poems.models import Poem
 from articles.models import Article
 from lifeX.models import LifeXWeek
 from brainbank.models import BrainBankIdea
-
-from itertools import chain
-from operator import attrgetter
 from sitedata.social_meta import create_meta
 
-def home(request, page=''):
-	try:
-		blogs = BlogPost.objects.order_by('-published')[:3]
-		stories = StoryPost.objects.order_by('-published')[:3]
-		poems = Poem.objects.order_by('-published')[:3]
-		articles = Article.objects.order_by('-published')[:3]
-		brainbank_idea = BrainBankIdea.objects.latest('published')
-		lifeXweek = LifeXWeek.objects.latest('number')
-		description = 'The personal website of Harshvardhan Pandit (coolharsh55)'
-		keywords = ['harshp.com','blog','stories','poems',]
-		meta = create_meta('harshp.com', description, keywords, url=request.build_absolute_uri(),)
-	except (BlogPost.DoesNotExist,
-			StoryPost.DoesNotExist,
-			Poem.DoesNotExist,
-			Article.DoesNotExist,
-			LifeXWeek.DoesNotExist,
-			BrainBankIdea.DoesNotExist):
-		raise Http404('Error retrieving website data...')
-	return render_to_response('harshp/index.html', {
-		'blogs':blogs, 'stories':stories, 'poems':poems, 'articles':articles,
-		'brainbank_idea':brainbank_idea, 'lifeXweek':lifeXweek, 'meta':meta,})
+
+def home(request):
+    """homepage for harshp.com
+
+    show all the latest posts in different apps
+
+    Args:
+        request(HttpResponse)
+
+    Returns:
+        HttpResponse: 200 on success, 404 on error
+
+    Raises:
+        Http404: error retrieving posts
+    """
+    try:
+        blogs = BlogPost.objects.order_by('-published')[:3]
+        stories = StoryPost.objects.order_by('-published')[:3]
+        poems = Poem.objects.order_by('-published')[:3]
+        articles = Article.objects.order_by('-published')[:3]
+        brainbank_idea = BrainBankIdea.objects.latest('published')
+        lifexweek = LifeXWeek.objects.latest('number')
+        description = """
+            The personal website of Harshvardhan Pandit (coolharsh55)"""
+        keywords = ['harshp.com', 'blog', 'stories', 'poems', ]
+        meta = create_meta(
+            'harshp.com',
+            description,
+            keywords,
+            url=request.build_absolute_uri(),
+        )
+    except (BlogPost.DoesNotExist,
+            StoryPost.DoesNotExist,
+            Poem.DoesNotExist,
+            Article.DoesNotExist,
+            LifeXWeek.DoesNotExist,
+            BrainBankIdea.DoesNotExist):
+        raise Http404('Error retrieving website data...')
+    return render_to_response(
+        'harshp/index.html',
+        {
+            'blogs': blogs,
+            'stories': stories,
+            'poems': poems,
+            'articles': articles,
+            'brainbank_idea': brainbank_idea,
+            'lifeXweek': lifexweek,
+            'meta': meta,
+        }
+    )
+
 
 def changelog(request):
-	return render_to_response('harshp/changelog.html')
+    """changelog
+
+    show the project changelog
+
+    Args:
+        request(HttpResponse)
+
+    Returns:
+        HttpResponse: 200 on success, 404 on error
+
+    Raises:
+        None
+    """
+    return render_to_response('harshp/changelog.html')
+
 
 def privacypolicy(request):
-	return render_to_response('harshp/privacypolicy.html')
+    """privacy policy
+
+    show the privacy policy for website
+
+    Args:
+        request(HttpResponse)
+
+    Returns:
+        HttpResponse: 200 on success, 404 on error
+
+    Raises:
+        None
+    """
+    return render_to_response('harshp/privacypolicy.html')
