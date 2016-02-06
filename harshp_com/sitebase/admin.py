8e840d70d6912eb13b9e1ad50f93ccb9ae156c73
Harshvardhan Pandit <coolharsh55@gmail.com>
2016-02-06 20:54:20 +0000

    new:admin for sitebase models
    Added sitebase models Author and Tag.
    
    Signed-off-by: Harshvardhan Pandit <coolharsh55@gmail.com>

--- harshp_com/sitebase/admin.py
+++ harshp_com/sitebase/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author
+from .models import Tag
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'email']
+    ordering = ['name']
+    search_fields = [
+        'name', 'email', 'short_description', 'long_description']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'description']
+    ordering = ['name']
+    search_fields = ['name', 'description']
