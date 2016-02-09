32d7e604780ebce29eb713807fc2fa5d1e3cf442
Harshvardhan Pandit <coolharsh55@gmail.com>
2016-02-09 16:16:51 +0000

    new: add templates directory and settings
    All templates are now under a common directory called 'templates'.
    
    Signed-off-by: Harshvardhan Pandit <coolharsh55@gmail.com>

--- harshp_com/harshp_com/settings/templates.py
+++ harshp_com/harshp_com/settings/templates.py
@@ -4,6 +4,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
