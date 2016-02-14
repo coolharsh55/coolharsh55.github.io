7bbcac3efd255a89f325d0b59c937e7d724c732e
Harshvardhan Pandit <coolharsh55@gmail.com>
2016-02-14 00:55:52 +0000

    new:homepage stub url and view
    Added a stub for the url and view under harshp_com
    
    Signed-off-by: Harshvardhan Pandit <coolharsh55@gmail.com>

--- harshp_com/harshp_com/urls.py
+++ harshp_com/harshp_com/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'', include('sitebase.urls')),
     url(r'', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
