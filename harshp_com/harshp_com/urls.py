8c0493f3f5519b92867a2c34a8f418c0afadaddb
Harshvardhan Pandit <coolharsh55@gmail.com>
2016-03-16 18:10:45 +0000

    new:poems app with models,urls,views
    Added the poems app with model - Poem, associated urls and views for homepage and poem.
    
    Signed-off-by: Harshvardhan Pandit <coolharsh55@gmail.com>

--- harshp_com/harshp_com/urls.py
+++ harshp_com/harshp_com/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     # apps
     url(r'', include('blog.urls')),
     url(r'', include('me.urls')),
+    url(r'', include('poems.urls')),
     url(r'', include('stories.urls')),
     # admin
     url(r'', include('harshp_com.adminurls')),
