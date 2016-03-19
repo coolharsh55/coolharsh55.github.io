162ec67b0e67ace5dfcb721b3b802ae44c4962c0
Harshvardhan Pandit <coolharsh55@gmail.com>
2016-03-19 15:49:53 +0000

    new:Added admin for Poem
    Added admin for Poem model from Poem app.
    
    Signed-off-by: Harshvardhan Pandit <coolharsh55@gmail.com>

--- harshp_com/poems/admin.py
+++ harshp_com/poems/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from sitebase.admin import PostAdmin
+
+from .models import Poem
+
+
+@admin.register(Poem)
+class PoemAdmin(PostAdmin):
+    """admin for Poem"""
+
+    fieldsets = [
+        ('info', {
+            'fields': [
+                'title', 'authors', 'short_description', 'slug'],
+        }),
+        ('publish', {
+            'fields': [
+                'date_created', 'date_published',
+                'is_published', 'highlight'],
+        }),
+        ('content', {
+            'classes': ('wide',),
+            'fields': ['body_type', 'body'],
+        }),
+        ('html', {
+            'classes': ('wide',),
+            'fields': ['body_text'],
+        }),
+        ('meta', {
+            'fields': ['headerimage', 'tags'],
+        }),
+    ]
+    list_display = (
+        'title',
+        'date_published', 'date_updated', 'highlight', 'is_published')
+    list_display_editable = ('is_published', 'highlight')
+    list_display_links = (
+        'title', 'date_published', 'date_updated')
+    list_filter = ['is_published', 'authors', 'tags']
+    ordering = (
+        'title',
+        'date_published', 'date_updated',
+        'highlight', 'is_published')
+    search_fields = ['title', 'short_description', 'series__short_description']
