4043341e823b5441ae9e7b6d91cb0db012f6e0e6
Harshvardhan Pandit <coolharsh55@gmail.com>
2016-10-02 12:30:43 +0100

    research homepage, publications and stub
    Added research homepage, publications, sections for phd, msc, bengg and more stubs.
    
    Signed-off-by: Harshvardhan Pandit <coolharsh55@gmail.com>

--- harshp_com/research/urls.py
+++ harshp_com/research/urls.py
@@ -4,11 +4,31 @@ from django.conf.urls import include, url
 
 from . import views
 
+phd_urlpatterns = [
+    url(r'^$', views.phd_home, name='home'),
+    url(r'^directed-study/$', views.phd_directed_study, name='directed-study')
+]
+
+msc_urlpatterns = [
+    url(r'^$', views.msc_home, name='home')
+]
+
+b_engg_urlpatterns = [
+    url(r'^$', views.b_engg_home, name='home')
+]
+
 research_urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^publications/$', views.publications, name='publications'),
+    url(r'^interests/$', views.interests, name='interests'),
+    url(r'^phd/', include(phd_urlpatterns, namespace='phd')),
+    url(r'^msc/', include(msc_urlpatterns, namespace='msc')),
+    url(r'^b-engg/', include(b_engg_urlpatterns, namespace='b-engg'))
 ]
 
 urlpatterns = [
     url(r'', include('harshp_com.urls_commons')),
     url(r'', include(research_urlpatterns, namespace='research')),
 ]
+
+handler404 = 'harshp_com.views.handler404'
--- harshp_com/research/views.py
+++ harshp_com/research/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'research/homepage.html')
+
+
+def publications(request):
+    return render(request, 'research/publications.html')
+
+
+def interests(request):
+    return render(request, 'research/interests.html')
+
+
+def phd_home(request):
+    return render(request, 'research/phd/homepage.html')
+
+
+def phd_directed_study(request):
+    return render(request, 'research/phd/directed_study.html')
+
+
+def msc_home(request):
+    return render(request, 'research/msc/homepage.html')
+
+
+def b_engg_home(request):
+    return render(request, 'research/b_engg/homepage.html')
