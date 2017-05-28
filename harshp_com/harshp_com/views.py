fdc6536499ad5d5e5a2a15bce3f1dff0b1b317b1
Harshvardhan Pandit <me@harshp.com>
2017-05-28 03:51:47 +0100

    fix handler404: caused server 500

--- harshp_com/harshp_com/views.py
+++ harshp_com/harshp_com/views.py
@@ -82,17 +82,13 @@ def privacy_policy(request):
 
 
 def handler404(request):
-    response = render(
-        request, 'sitebase/404.html', {},
-        context_instance=RequestContext(request))
+    response = render(request, 'sitebase/404.html', {})
     response.status_code = 404
     return response
 
 
 def handler500(request):
-    response = render(
-        request, 'sitebase/500.html', {},
-        context_instance=RequestContext(request))
+    response = render(request, 'sitebase/500.html', {})
     response.status_code = 500
     return response
 
