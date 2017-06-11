fed26c3f4517815674e9e19fd7291a27a0951de0
Harshvardhan Pandit <me@harshp.com>
2017-06-11 12:17:56 +0100

    script to sync database

---
+++ db_sync.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Retrieve postgresql backup from server
+echo "retrieving db backup from server..."
+scp harshp.com:~/backup/harshp_com_db_backup.sql /tmp/harshp_com_db_backup.sql
+if [ $? -eq 0 ]; then
+    echo "success!"
+else
+    rm /tmp/harshp_com_db_backup.sql
+    echo "failed!"
+    exit -1
+fi
+
+# Clear existing database
+sudo -u postgres psql -c "drop database if exists harshp_com;"
+sudo -u postgres psql -c "create database \"harshp_com\";"
+sudo -u postgres psql -c \
+    "grant all privileges on database \"harshp_com\" to harshp_db_admin;"
+sudo -u postgres pg_restore -U harshp_db_admin -d harshp_com -v \
+    /tmp/harshp_com_db_backup.sql
+rm /tmp/harshp_com_db_backup.sql
+echo "updated database"
+
+# Apply any unapplied migrations
+python3 harshp_com/manage.py migrate
+
+exit 0
