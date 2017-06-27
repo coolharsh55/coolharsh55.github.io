593b6e71cdf5b0f04970056f51aa20aefff7c0cd
Harshvardan Pandit <coolharsh55@gmail.com>
2017-06-27 17:06:13 +0100

    change:admin theme settings for boolean display bug

--- harshp_com/harshp_com/settings/admin.py
+++ harshp_com/harshp_com/settings/admin.py
@@ -4,30 +4,30 @@
 # THEMES
 ########################
 
-JET_THEMES = [
-    {
-        'theme': 'light-green',
-        'color': '#2faa60',
-        'title': 'Light Green'
-    },
-    {
-        'theme': 'light-violet',
-        'color': '#a464c4',
-        'title': 'Light Violet'
-    },
-    {
-        'theme': 'light-blue',
-        'color': '#5EADDE',
-        'title': 'Light Blue'
-    },
-    {
-        'theme': 'light-gray',
-        'color': '#222',
-        'title': 'Light Gray'
-    }
-]
+#JET_THEMES = [
+#    {
+#        'theme': 'light-green',
+#        'color': '#2faa60',
+#        'title': 'Light Green'
+#    },
+#    {
+#        'theme': 'light-violet',
+#        'color': '#a464c4',
+#        'title': 'Light Violet'
+#    },
+#    {
+#        'theme': 'light-blue',
+#        'color': '#5EADDE',
+#        'title': 'Light Blue'
+#    },
+#    {
+#        'theme': 'light-gray',
+#        'color': '#222',
+#        'title': 'Light Gray'
+#    }
+#]
 
-JET_DEFAULT_THEME = 'light-blue'
+#JET_DEFAULT_THEME = 'light-blue'
 
 ########################
 # SIDEBAR
@@ -35,7 +35,7 @@ JET_DEFAULT_THEME = 'light-blue'
 
 # whether to use compact (one-level) menu
 # will show all apps in single column
-JET_SIDE_MENU_COMPACT = True
+#JET_SIDE_MENU_COMPACT = True
 
 # apps to show in the sidebar
 #JET_SIDE_MENU_CUSTOM_APPS = [
