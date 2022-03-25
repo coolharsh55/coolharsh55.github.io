a36c0e7278c4f90903c760761c2835cd15762699
Harshvardhan Pandit <me@harshp.com>
2022-03-25 08:28:44 +0000

    books: updates

--- code/books_read_dates.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-dates=$(grep -oP '[^#]\s+hpcom:date_book_read "\K([0-9\-]+)' content/hobbies/books.ttl) ;
-year_max=$(date +%Y)
-for i in $(seq 1995 $year_max) ; do
-    echo "$i :: $(echo $dates | grep -o $i | wc -w)" ;
-done
