0ac512a62566fa280bce14e8ad6af27f8c7b5433
Harshvardhan Pandit <me@harshp.com>
2022-07-28 13:59:09 +0100

    books: read per month and projected per year

--- code/books_read_dates.sh
+++ code/books_read_dates.sh
@@ -3,5 +3,12 @@
 dates=$(grep -oP '[^#]\s+hpcom:date_book_read "\K([0-9\-]+)' content/hobbies/books.ttl) ;
 year_max=$(date +%Y)
 for i in $(seq 1995 $year_max) ; do
-    echo "$i :: $(echo $dates | grep -o $i | wc -w)" ;
+    books_read=$(echo $dates | grep -o $i | wc -w)
+    echo "$i :: $books_read" ;
 done
+current_month=$(date +%m)
+books_per_month=$((books_read/current_month))
+projected_books_read_year=$((books_per_month*12))
+echo "books read per month this year: $books_per_month"
+echo "projected books read this year: $projected_books_read_year"
+
